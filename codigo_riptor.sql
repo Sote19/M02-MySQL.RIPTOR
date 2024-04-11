311db5729351439a4c86edf1d53c4ffbbe4cc711
Soteras <156694267+Sote19@users.noreply.github.com>
2024-04-11 19:03:43 +0200

    codigo_riptor.sql

--- codigo_riptor.sql
+++ codigo_riptor.sql
@@ -1,8 +1,13 @@
 CREATE DATABASE Riptor;
 USE Riptor;
 
+CREATE TABLE Categorias (
+id_cat INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+nombre_cat VARCHAR(45),
+descripcion_cat VARCHAR(100)
+);
 CREATE TABLE Productos (
-id_prod INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+id_producto INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 nombre VARCHAR(100) NOT NULL,
 precio FLOAT NOT NULL,
 color VARCHAR(20) NOT NULL,
@@ -10,8 +15,10 @@ marca VARCHAR(30) NOT NULL,
 stock INT NOT NULL,
 descripcion VARCHAR(100) NOT NULL,
 categoria VARCHAR(30) NOT NULL,
-materiales VARCHAR(45) NOT NULL
+materiales VARCHAR(45) NOT NULL,
+id_cat INT /*FK*/ NOT NULL
 );
+ALTER TABLE Productos ADD CONSTRAINT fk_cat_2 FOREIGN KEY (id_cat) REFERENCES Categorias(id_cat);
 
 CREATE TABLE Usuarios (
 id_usu INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -34,7 +41,7 @@ id_vent INT PRIMARY KEY NOT NULL,
 id_usu INT /*FK*/ NOT NULL,
 cantidadprod_vent INT NOT NULL,
 descuento_vent FLOAT NOT NULL,
-preciototal_vent FLOAT NOT NULL
+preiototal_vent FLOAT NOT NULL
 );
 ALTER TABLE Ventas ADD CONSTRAINT fk_usu FOREIGN KEY (id_usu) REFERENCES Usuarios(id_usu);
 
@@ -54,10 +61,10 @@ CREATE TABLE Devolucion (
 id_dev INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 fecha_dev DATETIME NOT NULL,
 motivo_dev VARCHAR(50) NOT NULL,
-id_prod INT NOT NULL /*FK*/,
+id_producto INT NOT NULL /*FK*/,
 id_vent INT NOT NULL /*FK*/
 );
-ALTER TABLE Devolucion ADD CONSTRAINT fk_producto_9 FOREIGN KEY (id_prod) REFERENCES Productos(id_prod);
+ALTER TABLE Devolucion ADD CONSTRAINT fk_producto_9 FOREIGN KEY (id_producto) REFERENCES Productos(id_producto);
 ALTER TABLE Devolucion ADD CONSTRAINT fk_vent FOREIGN KEY (id_vent) REFERENCES Ventas(id_vent);
 
 CREATE TABLE Proveedor (
@@ -67,21 +74,9 @@ nombre_prov VARCHAR(100) NOT NULL,
 telefono_prov INT(12) NOT NULL,
 direccion_prov VARCHAR(100) NOT NULL,
 electronico_prov VARCHAR(100) NOT NULL,
-id_prod INT NOT NULL /*FK*/
+id_cat INT NOT NULL /*FK*/
 );
-ALTER TABLE Proveedor ADD CONSTRAINT fk_producto_7 FOREIGN KEY (id_prod) REFERENCES Productos(id_prod);
-
-CREATE TABLE Categorias (
-id_cat INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-nombre_cat VARCHAR(45),
-descripcion_cat VARCHAR(100)
-);
-
-CREATE TABLE Subcategorias (
-id_subcat INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-nombre_subcat VARCHAR(45),
-descripcion_subcat VARCHAR(100)
-);
+ALTER TABLE Proveedor ADD CONSTRAINT fk_cat_1 FOREIGN KEY (id_cat) REFERENCES Categorias(id_cat);
 
 CREATE TABLE Sudaderas (
 id_suda INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -89,9 +84,9 @@ talla_suda VARCHAR(5) NOT NULL,
 capucha_suda TINYINT(1) NOT NULL,
 tipobolsillo_suda VARCHAR(30) NOT NULL,
 corte_suda VARCHAR(30) NOT NULL,
-id_prod INT /*FK*/ NOT NULL 
+id_producto INT /*FK*/ NOT NULL 
 );
-ALTER TABLE Sudaderas ADD CONSTRAINT fk_producto_6 FOREIGN KEY (id_prod) REFERENCES Productos(id_prod);
+ALTER TABLE Sudaderas ADD CONSTRAINT fk_producto_6 FOREIGN KEY (id_producto) REFERENCES Productos(id_producto);
 
 CREATE TABLE Pantalones (
 id_pant INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -99,75 +94,59 @@ talla_pant VARCHAR(10) NOT NULL,
 cierre_pant VARCHAR(30) NOT NULL,
 corte_pant VARCHAR(30) NOT NULL,
 bolsillos_pant INT(10) NOT NULL,
-id_prod INT NOT NULL /*FK*/
+id_producto INT NOT NULL /*FK*/
 );
-ALTER TABLE Pantalones ADD CONSTRAINT fk_producto_5 FOREIGN KEY (id_prod) REFERENCES Productos(id_prod);
+ALTER TABLE Pantalones ADD CONSTRAINT fk_producto_5 FOREIGN KEY (id_producto) REFERENCES Productos(id_producto);
 
 CREATE TABLE Chaquetas (
 id_chaq INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 talla_chaq INT(10) NOT NULL,
 capucha_chaq TINYINT(1) NOT NULL,
 corte_chaq VARCHAR(30) NOT NULL,
-id_prod INT NOT NULL /*FK*/
+id_producto INT NOT NULL /*FK*/
 );
-ALTER TABLE Chaquetas ADD CONSTRAINT fk_producto_4 FOREIGN KEY (id_prod) REFERENCES Productos(id_prod);
+ALTER TABLE Chaquetas ADD CONSTRAINT fk_producto_4 FOREIGN KEY (id_producto) REFERENCES Productos(id_producto);
 
 CREATE TABLE Camisetas (
 id_cami INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 talla_cami VARCHAR(10) NOT NULL,
 corte_cami VARCHAR(30) NOT NULL,
-id_prod INT NOT NULL /*FK*/
+id_producto INT NOT NULL /*FK*/
 );
-ALTER TABLE Camisetas ADD CONSTRAINT fk_producto_3 FOREIGN KEY (id_prod) REFERENCES Productos(id_prod);
+ALTER TABLE Camisetas ADD CONSTRAINT fk_producto_3 FOREIGN KEY (id_producto) REFERENCES Productos(id_producto);
 
 CREATE TABLE Ropa_interior (
 id_inter INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 talla_lenceria_inter VARCHAR(45) NOT NULL,
 conjunto_inter TINYINT(1) NOT NULL,
-id_prod INT NOT NULL /*FK*/
+id_producto INT NOT NULL /*FK*/
 );
-ALTER TABLE Ropa_interior ADD CONSTRAINT fk_producto_2 FOREIGN KEY (id_prod) REFERENCES Productos(id_prod);
+ALTER TABLE Ropa_interior ADD CONSTRAINT fk_producto_2 FOREIGN KEY (id_producto) REFERENCES Productos(id_producto);
 
 CREATE TABLE Zapatillas (
 id_zapa INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 tallaje_zapa FLOAT NOT NULL,
 material_suela_zapa VARCHAR(30) NOT NULL,
-id_prod INT NOT NULL /*FK*/
+id_producto INT NOT NULL /*FK*/
 );
-ALTER TABLE Zapatillas ADD CONSTRAINT fk_producto_1 FOREIGN KEY (id_prod) REFERENCES Productos(id_prod);
+ALTER TABLE Zapatillas ADD CONSTRAINT fk_producto_1 FOREIGN KEY (id_producto) REFERENCES Productos(id_producto);
 
 CREATE TABLE Gorras_Gorros (
 id_gorr INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 talla_gorr FLOAT NOT NULL,
 visera_gorr TINYINT(1) NOT NULL,
 cierre_gorr VARCHAR(15) NOT NULL,
-id_prod INT NOT NULL /*FK*/
+id_producto INT NOT NULL /*FK*/
 );
-ALTER TABLE Gorras_Gorros ADD CONSTRAINT fk_producto FOREIGN KEY (id_prod) REFERENCES Productos(id_prod);
+ALTER TABLE Gorras_Gorros ADD CONSTRAINT fk_producto FOREIGN KEY (id_producto) REFERENCES Productos(id_producto);
 
 CREATE TABLE Vent_Prod(
-id_prod INT,
+id_producto INT,
 id_vent INT,
 cantidad INT,
-PRIMARY KEY(id_prod, id_vent),
-FOREIGN KEY (id_prod) REFERENCES Productos(id_prod),
+PRIMARY KEY(id_producto, id_vent),
+FOREIGN KEY (id_producto) REFERENCES Productos(id_producto),
 FOREIGN KEY (id_vent) REFERENCES Ventas(id_vent)
-);
-
-CREATE TABLE Prod_Sub(
-id_prod INT,
-id_subcat INT,
-PRIMARY KEY(id_prod, id_subcat),
-FOREIGN KEY (id_prod) REFERENCES Productos(id_prod),
-FOREIGN KEY (id_subcat) REFERENCES Subcategorias(id_subcat)
-);
-
-CREATE TABLE Cat_Subcat(
-id_cat INT,
-id_subcat INT,
-PRIMARY KEY(id_cat, id_subcat),
-FOREIGN KEY (id_cat) REFERENCES Categorias(id_cat),
-FOREIGN KEY (id_subcat) REFERENCES Subcategorias(id_subcat)
 );
 
 /*-----------------------INICIO TRIGGERS-------------------------------*/
@@ -185,9 +164,9 @@ BEGIN
     CREATE TRIGGER trig_Adios_privilegios
     BEFORE DELETE ON Productos
     FOR EACH ROW
-    BEGIN    
-		CALL Revoke_Privileges(CURRENT_USER());
-	END
+BEGIN    
+	CALL Revoke_Privileges(CURRENT_USER());
+    END
 // DELIMITER ;
 
 /*TRIGGER*/
@@ -199,9 +178,9 @@ FOR EACH ROW
 BEGIN
     UPDATE Productos
     SET stock = stock - NEW.cantidad
-    WHERE id_prod = NEW.id_prod;
-END;
-// DELIMITER ;
+    WHERE id_producto = NEW.id_producto;
+END// 
+DELIMITER ;
 
 /*TRIGGER*/
 USE Riptor;
@@ -211,21 +190,8 @@ AFTER INSERT ON Devolucion
 FOR EACH ROW
 BEGIN
     SIGNAL SQLSTATE '54000' SET MESSAGE_TEXT = 'La devolución ha sido registrada correctamente';
-END;
+END
 // DELIMITER ;
 /*-----------------------FIN TRIGGERS-------------------------------*/
-
-/*-----------------------QUERY'S-------------------------------*/
-
 /*-----------------------INICIO INDEX'S-------------------------------*/
-CREATE INDEX idx_Agil_core ON Productos(id_prod, Stock); /*Hemos pensado que era el mas basico, ya que agiliza la primary key del core también en stock, cuando el usuario compra, para saber si hay stock de x articulo, pensamos que es una busqueda que se realiza muchas veces*/
-CREATE INDEX idx_Mejora_relacion ON Vent_Prod (id_prod, id_vent); /*Mejora todo el trafico entre ventas y productos*/
-CREATE INDEX idx_categorias ON Categorias (nombre_cat); /*Para busquedas mas rapidas, entre productos y categorias*/
 /*-----------------------FIN INDEX'S-------------------------------*/
-
-/*-----------------------1K INSERTS-------------------------------*/
-
-/*-----------------------5K INSERTS-------------------------------*/
-
-/*-----------------------10K INSERTS-------------------------------*/
-
